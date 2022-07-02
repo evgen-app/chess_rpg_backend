ba0c8499e092e55983303274e10263cdf6f7c0c8
Alexander-D-Karpov <alexandr.d.karpov@gmail.com>
2022-07-02 14:20:58 +0300

    Removed files for stable branch

--- room/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    path("room/", consumers.QueueConsumer.as_asgi()),
-    path("room/<str:room_name>", consumers.RoomConsumer.as_asgi()),
-]
